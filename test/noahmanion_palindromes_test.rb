ac2660129d7db7469b47219f930fbb4e2fdf2aee
Noah Manion <noahmanion@Noahs-MacBook-Pro.local>
2019-04-30 18:24:42 -0500

    Tests pass. p208

--- test/noahmanion_palindromes_test.rb
+++ test/noahmanion_palindromes_test.rb
@@ -8,4 +8,16 @@ class NoahmanionPalindromesTest < Minitest::Test
   def test_it_does_something_useful
     assert true
   end
+
+  def test_non_palindrome
+  	assert !"Apple".palindrome?
+  end
+
+  def test_literal_palindrome
+  	assert "racecar".palindrome?
+  end
+
+  def test_caps_in_palindrome
+  	assert "Racecar".palindrome?
+  end
 end
